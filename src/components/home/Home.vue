859a6360b34bf664470d1c052fc63464a4005a44
0316lyh <2042971411@qq.com>
2023-11-13 17:34:22 +0800

    完善Home.vue

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -12,48 +12,29 @@
           <el-table
               :data="fromData"
               style="width: 100%">
-            <el-table-column
-                prop=""
-                label=" "
-                width="180px"
-            ></el-table-column>
+            <el-table-column prop="" label=" " width="180px"></el-table-column>
 
             <el-table-column label="图标" align="center">
               <template v-slot="scope">
                 <div class="block">
                   <el-avatar shape="square" :size="70"
-                             :src=" 'https://moqiang-navigation-1318187204.cos.ap-guangzhou.myqcloud.com/icon%2F' +  scope.row.id+ '.jpg'"/>
+                             :src="'https://moqiang-navigation-1318187204.cos.ap-guangzhou.myqcloud.com/icon%2F' +  scope.row.id+ '.jpg'"
+                             class="clickable-avatar"
+                             @click.native="toWebsite(scope.row.url)"/>
                 </div>
               </template>
             </el-table-column>
 
-            <el-table-column
-                prop="name"
-                label="网站名"
-            >
+            <el-table-column prop="name" label="网站名"></el-table-column>
+            <el-table-column prop="content" label="介绍"></el-table-column>
+            <el-table-column prop="url" label="地址">
+              <template v-slot="scope">
+                <el-link :href="scope.row.url" target="_blank">{{ delWebsitePrefix(scope.row.url) }}</el-link>
+              </template>
             </el-table-column>
+            <el-table-column align="center" prop="liked" label="点赞数"></el-table-column>
 
-            <el-table-column
-                prop="content"
-                label="介绍"
-            >
-            </el-table-column>
-            <el-table-column
-                prop="url"
-                label="地址"
-            >
-            </el-table-column>
-            <el-table-column
-                align="center"
-                prop="liked"
-                label="点赞数"
-            >
-
-            </el-table-column>
-            <el-table-column
-                prop=""
-                label="点赞"
-            >
+            <el-table-column prop="" label="点赞">
               <template v-slot="scope">
                 <el-row>
                   <el-button type="primary" icon="el-icon-top" @click="addLiked(scope.row.id)"></el-button>
@@ -61,12 +42,7 @@
               </template>
             </el-table-column>
 
-            <el-table-column
-                prop=""
-                label=" "
-                width="180px"
-            >
-            </el-table-column>
+            <el-table-column prop="" label=" " width="180px"></el-table-column>
 
           </el-table>
         </div>
@@ -98,23 +74,38 @@ export default {
     this.getAll();
   },
   methods: {
+    // 获取所有网站
     getAll() {
-      axios.get('/website/getTopByLiked/' + this.topNum).then((res) => {
+      axios.get('/website/getTopByLiked/').then((res) => {
         this.fromData = res.data.data;
         console.log(this.fromData);
       })
     },
+    // 点赞
     addLiked(id) {
       axios.put('/website/addLiked/' + id).then((res) => {
         console.log(id);
         this.getAll();
       })
+    },
+    // 跳转到网站
+    toWebsite(url) {
+      console.log(url);
+      // 替换成您希望跳转的 URL
+      window.open(url);
+    },
+    // 删除网站前缀
+    delWebsitePrefix(url) {
+      // "https://javaguide.cn" ==> "javaguide.cn"
+      return url.replace(/(^\w+:|^)\/\//, '');
     }
   }
 }
 </script>
 
 <style scoped>
-
+.clickable-avatar {
+  cursor: pointer;
+}
 
 </style>
